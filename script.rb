3095f9d7b3604d266988cad9b33f012a8feb60c3
Sebastian Morr <sebastian@morr.cc>
2020-06-25 21:45:32 +0200

    Mask mentions and hashtags, unescape HTML entities
    Co-authored-by: Winston Smith <wsbraves@gmail.com>

--- script.rb
+++ script.rb
@@ -1,32 +1,44 @@
 require "json"
-
-#csv = IO.readlines "tweets.csv"
-#p csv.size
+require "cgi"
 
 js = File.read("tweet.js")
 json = js[25..-1]
 json = JSON.parse(json)
 
-langs = json.map do |j|
-  j["tweet"]["lang"]
-end
-
-#p langs.group_by{|e| e}.map{|k, v| [k, v.length]}.to_h
-
-json = json.select do |j|
+json.select! do |j|
   j["tweet"]["lang"] == "en"
 end
 
 tweets = json.map do |j|
-  j["tweet"]["full_text"]
+  urls = j["tweet"]["entities"]["urls"]
+  urls_lookup = {}
+  urls.each do |u|
+    urls_lookup[u["url"]] = u["expanded_url"]
+  end
+  j["tweet"]["full_text"].gsub(/https?:\/\/t.co\/\w+/) do |u|
+    urls_lookup[u] || "[media]"
+  end
 end
 
-tweets = tweets.select do |t|
-  not t =~ /^RT / and not t =~ /^@/
+tweets.select! do |t|
+  # Drop tweets that are RTs or replies to other people.
+  not t =~ /^RT / and not(t =~ /^@/ and not t =~ /^@blinry /)
 end
+
+tweets.map! do |t|
+  # Unescape HTML entities.
+  t = CGI.unescapeHTML(t)
+
+  # Because we only want top-level tweets, remove the mentions from longer threads.
+  t = t.gsub(/^@blinry /, "")
+
+  # Mask mentions and hashtags with a word joiner, U+2060.
+  t = t.gsub(/@/, "@⁠")
+  t = t.gsub(/#/, "#⁠")
+end
+
+tweets.shuffle!
 
 tweets.each do |t|
-  print "<|startoftext|>"
-  print t
-  print "<|endoftext|>\n"
+  puts "<|startoftext|>#{t}<|endoftext|>"
 end
